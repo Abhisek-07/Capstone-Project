e9109ea4f6124b5039faa6efc34d4ebaefd77ede
Abhisek Dash <abhisekdash6931@gmail.com>
2023-04-10 02:33:03 +0530

    updated and added new files

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -13,6 +13,7 @@ from streamlit_option_menu import option_menu
 
 heart_disease_model = pickle.load(open('C:/Users/91801/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav', 'rb'))
 # similarly load kidney disease model ...
+kidney_disease_model = pickle.load(open('C:/Users/91801/Desktop/Multiple Disease Prediction System/saved models/kidney_disease_model.sav', 'rb'))
 
 
 # sidebar for navigation
@@ -74,5 +75,33 @@ if(selected == 'Kidney Disease Prediction'):
     # page title
     st.title('Kidney Disease Prediction Using Machine Learning')
     
+    age = st.number_input('Age')
+    bp = st.number_input('Blood Pressure')
+    al = st.number_input('Albumin (nominal values from 0 to 5)')
+    sc = st.number_input('Serum creatinine')
+    hemo = st.number_input('Haemoglobin')
+    rbc = st.number_input('Red blood cells (1 : Normal; 0 : Abnormal)')
+    pc = st.number_input('Pus cells (1 : Normal; 0 : Abnormal)')
+    pcc = st.number_input('Pus cell clumps(1 : Present; 0 : Not present)')
+    pcv = st.number_input('Packed cell volume')
+    wc = st.number_input('White blood cell count')
+    rc = st.number_input('Red blood cell count')
+    appet = st.number_input('Appetite(1 : Poor; 0 : Good)')
+    ane = st.number_input('Anemia(1 : Yes; 0 : No)')
     
+    
+    
+    kidney_diagnosis = ''
+    
+    
+    if st.button('Kidney Disease Test Result'):
+        
+        kidney_disease_prediction = kidney_disease_model.predict([[age, bp, al, sc, hemo, rbc, pc, pcc, pcv, wc, rc, appet, ane]])
+        
+        if (kidney_disease_prediction[0] == 0):
+            kidney_diagnosis = 'The person has a kidney disease'
+        
+        else:
+            kidney_diagnosis = 'The person does not have a kidney disease'
 
+    st.success(kidney_diagnosis)
